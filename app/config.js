39783e1a834491cceeb701e70dea66e3a1e8b73b
LiuYi11437 <liuyi11437@outlook.com>
2020-07-26 16:15:36 +0800

    修改mongo地址

--- app/config.js
+++ app/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   secret: "zhihu-jwt-secret",
-  connectionStr:"mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
-    // process.env.NODE_ENV !== "development"
-      // ? "mongodb+srv://liuyi:liuyi123@liuyi-ojjbr.mongodb.net/test?retryWrites=true&w=majority"
-      // : "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false",
-  // connectionStr:
+  connectionStr:
+    process.env.NODE_ENV !== "development"
+      ? "mongodb+srv://liuyi:liuyi123@liuyi-ojjbr.mongodb.net/test?retryWrites=true&w=majority"
+      : "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false",
+  // connectionStr:"mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
   // "mongodb+srv://lewis:mukewang@zhihu-kag3y.mongodb.net/test?retryWrites=true",
 };
